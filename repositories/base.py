571b7517481b0909ddab36d54db000b08439d318
Konstantin <kostyakonov@gmail.com>
2024-11-27 15:37:25 +0300

    added comments

--- repositories/base.py
+++ repositories/base.py
@@ -9,6 +9,7 @@ class BaseRepository:
     def __init__(self, session):
         self.session = session
 
+    # get all hotels
     async def get_all(self):
         query = select(HotelsOrm)
         result = await self.session.execute(query)
@@ -16,25 +17,28 @@ class BaseRepository:
         return result.scalars().all()
 
 
+    # get one hotel by filter data
     async def get_one_or_none(self, **filter_by):
         query = select(self.model).filter_by(**filter_by)
         result = await self.session.execute(query)
 
         return result.scalars().one_or_none()
 
-
+    # add new hotel
     async def add(self, data: BaseModel):
         add_hotel_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
         res = await self.session.execute(add_hotel_stmt)
         return res.scalars().one()
 
 
+    # edit existing hotel
     async def edit(self, data: BaseModel, **filter_by):
         update_hotel_stmt = update(self.model).where(self.model.id==filter_by['id']).values(**data.model_dump())
         res = await self.session.execute(update_hotel_stmt)
         return None
 
 
+    # delete hotel
     async def delete(self, **filter_by):
         delete_hotel_stmt = delete(self.model).where(self.model.id==filter_by['id'])
         res = await self.session.execute(delete_hotel_stmt)
